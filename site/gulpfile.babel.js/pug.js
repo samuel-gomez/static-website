bdd47df901be15e06eda37a26a27cf4da386be30
samuel-gomez <contact@samuelgomez.fr>
2019-05-10 00:29:54 +0200

    blog layouts

--- site/gulpfile.babel.js/pug.js
+++ site/gulpfile.babel.js/pug.js
@@ -3,6 +3,7 @@ import { src, dest } from 'gulp';
 import pugg from 'pug';
 import pretty from 'pretty';
 import pug from 'gulp-pug';
+import debug from 'gulp-debug';
 import plumber from 'gulp-plumber';
 import fs from 'fs';
 import path from 'path';
@@ -26,11 +27,18 @@ const getFileData = fileName => JSON.parse(fs.readFileSync(`${baseData}${fileNam
 
 const getFolders = lang => {
   const all = fs.readdirSync(`src/pages/${lang}/blog`);
-  const folders = all.filter(folder => folder !== 'partials' && folder !== 'index.pug');
+  const folders = all.filter(
+    folder =>
+      folder !== 'partials' &&
+      folder !== '_layout.pug' &&
+      folder !== '_post.pug' &&
+      folder !== 'index.pug',
+  );
   const articles = [];
   folders.forEach(folder => {
     const article = require(`../src/pages/${lang}/blog/${folder}/partials/data.json`);
     article.link = `./${folder}`;
+    article.id = folder;
     articles.push(article);
   });
   return articles;
@@ -45,8 +53,6 @@ const pugTsk = () => {
     articles[lang] = getFolders(lang);
   });
 
-  console.log('ARTICLE', articles);
-
   const data = {
     base,
     general,
@@ -55,24 +61,24 @@ const pugTsk = () => {
     functions: { setClass, setClassActive, pugg, pretty },
     require,
     articles,
+    mypath: path.resolve(),
   };
 
-  return src([
-    `${pathSrc}/index.pug`,
-    `${pathSrc}/pages/**/**/*.pug`,
-    `!${pathSrc}/pages/**/**/_*.pug`,
-  ])
-    .pipe(plumber())
-    .pipe(
-      pug({
-        locals: data,
-        //debug: true,
-        //compileDebug: true,
-        //pretty: true,
-      }),
-    )
-    .pipe(dest(pathDest))
-    .pipe(reload({ stream: true }));
+  return (
+    src([`${pathSrc}/index.pug`, `${pathSrc}/pages/**/**/*.pug`, `!${pathSrc}/pages/**/**/_*.pug`])
+      .pipe(plumber())
+      //.pipe(debug())
+      .pipe(
+        pug({
+          locals: data,
+          //debug: true,
+          //compileDebug: true,
+          //pretty: true,
+        }),
+      )
+      .pipe(dest(pathDest))
+      .pipe(reload({ stream: true }))
+  );
 };
 
 export default pugTsk;
