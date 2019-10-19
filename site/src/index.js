64761617f9a8373fb9cb445279ddc903413b938f
samuel-gomez <contact@samuelgomez.fr>
2019-10-20 00:59:07 +0200

    feat(home) more light

--- site/src/index.js
+++ site/src/index.js
@@ -2,6 +2,7 @@ import Menu from './components/menu/menu';
 import Tabs from './components/tabs/tabs';
 import Demo from './components/demo/demo';
 import Smoke from './components/smoke/smoke';
+import Ray from './components/smoke/ray';
 import Technos from './components/technos/index';
 import Scrolling from './components/scroll/scrolling';
 import Card3d from './components/card3d/index';
@@ -11,12 +12,14 @@ const tabs = new Tabs('js-tabs');
 const tabsDemo = new Tabs('js-tabs--demo');
 const demo = new Demo();
 const smoke = new Smoke();
+const ray = new Ray();
 const technos = new Technos();
 const scrolling = new Scrolling();
 const card3d = new Card3d('js-article-text--home-skills');
 
 document.addEventListener('DOMContentLoaded', () => {
   smoke.init();
+  ray.init();
   menu.init();
   tabs.init();
   tabsDemo.init();
